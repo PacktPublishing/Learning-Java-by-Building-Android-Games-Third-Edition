ba2ba9342cc2cbe2ae677cdc05b8dbb087b33fe6
Shubham Sharma <60926838+shubham763@users.noreply.github.com>
2021-02-26 17:10:27 +0530

    Update PongActivity.java

--- Chapter 09/java/PongActivity.java
+++ Chapter 09/java/PongActivity.java
@@ -18,6 +18,7 @@ public class PongActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         Display display = getWindowManager().getDefaultDisplay();
         Point size = new Point();
