f52374668efa573d0ff2640d5af3b02163319fd0
Shubham Sharma <60926838+shubham763@users.noreply.github.com>
2021-02-26 17:09:58 +0530

    Update PongActivity.java

--- Chapter 08/C8Pong/PongActivity.java
+++ Chapter 08/C8Pong/PongActivity.java
@@ -12,5 +12,6 @@ public class PongActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 }
